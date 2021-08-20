c8a6900a1922dce918effcfef373a1d9a6555d41
Stelvio Alexandre Smith <silviosmith18@gmail.com>
2021-08-20 00:58:17 -0300

    Inserindo Js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
-let nome = window.document.getElementById('nome');
-let email = document.querySelector('#email');
+let nome = window.document.getElementById('nome')
+let email = window.document.querySelector('#email')
 let asssunto = document.querySelector('#assunto')
-
+let nomeOK= false
+let emailOK = false
+let asssuntoOK = false
+let mapa = document.querySelector("#mapa")
 
 nome.style.width = '100%'
 
@@ -10,26 +13,59 @@ email.style.width = '100%'
 asssunto.style.width = '100%'
 
 function validaNome (){
-    let txt = document.querySelector("#txtNome")
-
-    if(nome.Value.length<3){
-        
-        txt.innerHTML = "Nome Invalido"
-        txt.style.color = "red"
+    let txtNome = document.querySelector("#txtNome")
+   
+    if(nome.value.length<3){
+        txtNome.innerHTML = "Nome Invalido"
+        txtNome.innerHTML = "Nome Invalido"
+        txtNome.style.color = "red"
     
     } else{
-        txt.innerHTML="Nome Valido"
-        txt.style.color= "green"
+        txtNome.innerHTML="Nome Valido"
+        txtNome.style.color= "green"
+        nomeOK= true
     }
+}
+
+function validaEmail (){
+let txEmail = document.querySelector("#txtemail")
+
+if(email.value.indexof("@") == -1||email.value.indexof(".")==-1){
+txEmail.innerHTML = "E-mail Invalido"
+txEmail.style.color = "red"
+}else{
+    txEmail.innerHTML = "E-mail valido"
+    txEmail.style.color = "green"
+    emailOK= true
+}
 
 }
-function validaEmail(){
-    let txtEmail = document.querySelector("#txtEmail")
-    if(email.Value.indexof ("@")==-1 || email.Value.indexof(".")== -1 ){
-        txtEmail.innerHTML="Email Invalido"
-        txtEmail.style.color= "red"
-    } else{
-        txtEmail.innerHTML="Email valido"
-        txtEmail.style.color= "green"
+function validaAssunto(){
+
+    let txtAssunto = document.querySelector("#txtAssunto")
+    if(asssunto.value.length >=100){
+        txtAssunto.innerHTML = "Texto e muito grande, digite no maximo 100 caracteres "
+        txtAssunto.style.color="red"
+        txtAssunto.style.display = "block"
+    } else {
+        txtAssunto.style.display = "none"
+        asssuntoOK = true
+}
+    function enviar() {
+        if (nomeOK == true && emailOK == true && asssuntoOK == true){
+            alert ("Formulario enviado com Sucesso!")
+        }else {
+            alert( "Preencha o formulario corretamente antes de enviar ...")
+
+        }
     }
+    }
+function mapaZoom(){
+ mapa.style.width = "800px"
+ mapa.style.width = "600px"
+}
+
+function mapaNormal(){
+    mapa.style.width = "450px"
+    mapa.style.width = "350px"
 }
